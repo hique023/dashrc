0cc96f2ad0d8eac2670fac4d4b52f22fa60de302
hique023 <pedroviski10@hotmail.com>
2022-01-18 09:43:03 -0300

    Estilizado componente DashGlobal

--- src/components/CardGlobal/index.js
+++ src/components/CardGlobal/index.js
@@ -1,5 +1,6 @@
 // Global
 import React from "react";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 // Styles
 import "./styles.css";
@@ -7,8 +8,13 @@ import "./styles.css";
 export default function CardGlobal(props) {
   return (
     <div className="CardGlobal">
-      <h1>Total de {props.title}</h1>
-      <h2>{props.value}</h2>
+      <div className="iconChanel">{props.icon}</div>
+      <div className="contentCardGlobal">
+        <h1>Total de {props.title}</h1>
+        <h2>{props.value}</h2>
+        <h3>Em atendimento: {props.attendance}</h3>
+        <h3>Finalizados: {props.finished}</h3>
+      </div>
     </div>
   );
 }
--- src/components/DashGlobal/index.js
+++ src/components/DashGlobal/index.js
@@ -2,6 +2,9 @@
 import React from "react";
 import CardGlobal from "../CardGlobal";
 import Logo from "../../assets/logo.svg";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiComputerFill } from "react-icons/ri";
 
 // Styles
 import "./styles.css";
@@ -11,9 +14,27 @@ export default function Home() {
     <div className="containerDashGlobal">
       {/* <h1>Dashboard RC</h1> */}
       <div className="contentDashGlobal">
-        <CardGlobal title="Ligações" value="100" />
-        <CardGlobal title="Wpp" value="30" />
-        <CardGlobal title="Acessos" value="25" />
+        <CardGlobal
+          title="Ligações"
+          value="100"
+          icon={<BsFillTelephoneFill size={60} />}
+          attendance="3"
+          finished="22"
+        />
+        <CardGlobal
+          title="Chats"
+          value="30"
+          icon={<IoLogoWhatsapp size={60} />}
+          attendance="2"
+          finished="20"
+        />
+        <CardGlobal
+          title="Acessos"
+          value="25"
+          icon={<RiComputerFill size={60} />}
+          attendance="4"
+          finished="25"
+        />
         <div className="dashGlobalImg">
           <img src={Logo} alt="Logo de dash" />
         </div>
