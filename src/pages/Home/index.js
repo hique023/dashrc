8bf2db3f46a9a622fc1af354683348f0725274e2
hique023 <pedroviski10@hotmail.com>
2022-03-02 11:52:16 -0300

    Recuperado analistas do Firebase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,56 @@
 // Global
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DashGlobal from "../../components/DashGlobal";
 import Analist from "../../components/Analist";
+import firebase from "../../firebaseConfig.js";
 
 // Styles
 import "./styles.css";
 
 export default function Home() {
+  const [user, setUser] = useState({ data: [] });
+
+  const db = firebase.firestore();
+
+  function getUser() {
+    const count = [];
+
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          count.push(doc.data());
+        });
+
+        count.sort(function (a, b) {
+          if (a.name < b.name) {
+            return -1;
+          } else {
+            return true;
+          }
+        });
+
+        setUser({ data: count });
+      });
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="containerHome">
       <DashGlobal />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
-      <Analist />
+
+      {user.data.map((item, key) => (
+        <div className="containerAnalist" key={key}>
+          <Analist
+            name={item.name}
+            // email={item.email}
+            // active={item.active}
+          />
+        </div>
+      ))}
     </div>
   );
 }
